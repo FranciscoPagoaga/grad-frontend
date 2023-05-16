0cc0d12a0f3dc8b0b0c0c7326b64685443a01076
Francisco Pagoaga <francisco.pagaoga14@gmail.com>
2023-05-16 13:04:48 -0600

    Finished Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/logo.png";
 import { FaUserEdit } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import { useNavigate } from "react-router-dom";
